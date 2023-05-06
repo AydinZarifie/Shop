01725ef3df1807ea6256290d591e4fb7ab468ac2
unknown <aydinzarifieaszo@gmail.com>
2023-05-06 14:51:02 +0330

    Shop Project

---
+++ routes/admin.js
@@ -0,0 +1,22 @@
+const express = require("express");
+
+const router = express.Router();
+
+const isAuth = require('../middleware/is-auth');
+
+const adminController = require('../controllers/admin');
+
+
+router.get('/add-product', isAuth ,adminController.getAddProduct);
+
+router.post('/add-product',  isAuth ,adminController.postAddProduct);
+
+router.get('/products', isAuth , adminController.getProducts);
+
+router.get('/edit-product/:productId',  isAuth ,adminController.getEditProduct);
+
+router.post('/edit-product', isAuth , adminController.postEditProduct);
+
+router.post('/delete-product', isAuth , adminController.postDeleteProduct);
+
+module.exports = router;
